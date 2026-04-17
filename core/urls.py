78315527131a2481adbe6dcb065978bb95340093
abeni-hub <ebenezarsileshi@gmail.com>
2026-04-17 22:23:57 +0300

    add urls router

---
+++ core/urls.py
@@ -0,0 +1,29 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+from .views import *
+
+router = DefaultRouter()
+
+# CORE
+router.register(r'hero', HeroSectionViewSet)
+router.register(r'about', AboutUsViewSet)
+
+# SERVICES
+router.register(r'service-sections', ServiceSectionViewSet)
+router.register(r'services', ServiceViewSet)
+
+# HOW IT WORKS
+router.register(r'how-it-works', HowItWorksViewSet)
+router.register(r'steps', StepViewSet)
+
+# TESTIMONIALS
+router.register(r'testimonial-sections', TestimonialSectionViewSet)
+router.register(r'testimonials', TestimonialViewSet)
+
+# FEEDBACK
+router.register(r'feedback', FeedbackViewSet)
+
+# BLOG
+router.register(r'blogs', BlogViewSet)
+router.register(r'comments', CommentViewSet)
+
