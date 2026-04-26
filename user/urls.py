2573756c7eadd21350bde3cbe8e40db7bce8966a
abeni-hub <ebenezarsileshi@gmail.com>
2026-04-26 17:55:00 +0300

    updates on urls

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
-
-    # USERS
     path('users/', UserListView.as_view(), name='users-list'),
 ]
